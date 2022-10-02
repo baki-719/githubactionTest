af7b01fadf89bc8887586c55b509871b7a60f798
hgs6424 <hgs6424@naver.com>
2022-10-02 17:21:05 +0900

    initial commit

---
+++ src/main/kotlin/com/example/githubactiontest/GithubActionTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubactiontest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<GithubActionTestApplication>(*args)
+}
